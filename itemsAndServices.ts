b5431653b3602eb0ecaa080972c5dc2e6680198f
Anderson Andrad <anderson_andrade_@outlook.com>
2022-05-30 21:39:05 -0300

    feature: create base items and services

---
+++ itemsAndServices.ts
@@ -0,0 +1,37 @@
+type Items = {
+  name: string;
+  price: number;
+  typeTibar: TypesTibar;
+  weight: number;
+};
+
+type Clothes = {
+  name: string;
+  price: number;
+  typeTibar: TypesTibar;
+  weight: number;
+};
+
+type Animals = {
+  name: string;
+  price: number;
+  typeTibar: TypesTibar;
+};
+
+type Vehicles = {
+  name: string;
+  price: number;
+  typeTibar: TypesTibar;
+};
+
+type Driving = {
+  name: string;
+  pricePerKM: number;
+  typeTibar: TypesTibar;
+};
+
+type Services = {
+  name: string;
+  price: number;
+  typeTibar: TypesTibar;
+};
